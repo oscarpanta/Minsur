fc8861080e5c1946a313148fda83786130b85729
oscar panta <opanta@migrago.com>
2025-02-09 02:18:35 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,8 +17,8 @@ const routes: Routes = [
 
       { path: '', redirectTo: 'home', pathMatch: 'full' }
     ]
-  }
-
+  },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
