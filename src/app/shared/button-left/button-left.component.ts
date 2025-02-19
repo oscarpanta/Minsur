300755d967846865e4757bc93a1cba0277f9dab0
oscar panta <opanta@migrago.com>
2025-02-19 14:14:04 -0500

    upd

--- src/app/shared/button-left/button-left.component.ts
+++ src/app/shared/button-left/button-left.component.ts
@@ -9,11 +9,10 @@ export class ButtonLeftComponent {
   @Input() link: string = '/';
 
   @Input() defaultBorderColor: string = '#0063f9';
-  @Input() hoverBorderColor: string = '#7df900';
-
   @Input() defaultStrokeColor: string = '#0063f9';
   @Input() hoverStrokeColor: string = '#ffffff';
-
   @Input() label: string = 'Ver más';
   @Input() textColor: string = '#0063f9';
+  @Input() hoverBackgroundColor: string = 'transparent';
+
 }
